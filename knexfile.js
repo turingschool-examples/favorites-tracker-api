cc43f9044c925ea1de93c2368ff8df8c8fcf91f8
Robbie Jaeger <jaeger.rob@gmail.com>
2019-09-03 11:16:52 -0600

    Update knex connection table name

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/mod3_tracker',
+    connection: 'postgres://localhost/favoerites_tracker',
     useNullAsDefault: true
   }
 };
